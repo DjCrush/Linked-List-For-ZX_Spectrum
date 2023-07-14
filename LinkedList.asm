3cad907b2db4f3450b1daeeb1f304c343eabe6fe
Yevgeny Antonov <yevgeny1978@mail.ru>
2023-07-15 03:59:27 +0700

    Update LinkedList.asm
    Bug Fixes

--- LinkedList.asm
+++ LinkedList.asm
@@ -71,30 +71,45 @@ LL_DelNode5		dec	hl
 			ld	e, (hl)
 			ld	(AdrOfNode), hl ; Save the adress of the current node in the AdrOfNode variable
 			ex	de, hl
-			jr	LL_DelNode1		
+			jr	LL_DelNode1
+
+;*********************************************************************************************************
+; finding the last node in the list
+; in - hl contains the address of the variable that contains the address of the first node in the list
+; out - de - address of the last node in the linked list or NULL if there are no nodes in the linked list
+;*********************************************************************************************************
 LL_SearchLastNode	ld	e, (hl)
 			inc	hl
 			ld	d, (hl)
 			ex	de, hl
 LL_SearchLastNode1	ld	a, h
 			or	l
-			ret	z	; de - Last node of the Linked List
+			ret	z	; de - address of the last node in the linked list or NULL if there are no nodes in the linked list
 			ld	e, (hl)
 			inc	hl
 			ld	d, (hl)
 			ex	de, hl
 			jr	LL_SearchLastNode1
-LL_AddNode		ld	(LL_AddNode3 + 1), a
-			ld	ix, MemoryBuffer
-LL_AddNode1		ld	a, (ix + 0)
-			or	(ix + 1)
-			or	(ix + 2)
-			jr	z, LL_AddNode2	; if you can find a free memory location
-			inc	ix
-			jr	LL_AddNode1
-LL_AddNode2		call	LL_SearchLastNode  ; find the last element of the linked list . de - last
-			ex	de, hl		; hl - The pointer to the last Node of the linked list
-LL_AddNode3		ld	a, 0
+
+;*****************************************************************************************************
+; adds the node to the end of the list
+; in - hl contains the address of the variable that contains the address of the first node in the list
+; out - nothing
+;*****************************************************************************************************
+LL_AddNode		ld	(LL_AddNode4 + 1), a
+			ld	a, h
+			or	l
+			jr	nz, LL_AddNode1	; if a list exists 
+			call	LL_Malloc	; if a list is not exists, 
+			push	ix		; then allocate memory and create the first node
+			pop	de
+			ld	(hl), e		; now the pointer will point to the first node
+			inc	hl
+			ld	(hl), d
+			jr	LL_AddNode3
+LL_AddNode1		call	LL_Malloc
+LL_AddNode2		call	LL_SearchLastNode  ; find the last element of the linked list . de - adr of last node
+LL_AddNode3		ex	de, hl		; hl - The pointer to the last Node of the linked list
 			push	ix
 			pop	de
 			ld	(hl), d
@@ -105,8 +120,16 @@ LL_AddNode3		ld	a, 0
 			inc	hl
 			ld	(hl), 0
 			inc	hl
+LL_AddNode4		ld	a, 0
 			ld	(hl), a
 			ret
+LL_Malloc		ld	ix, MemoryBuffer
+LL_Malloc1		ld	a, (ix + 0)
+			or	(ix + 1)
+			or	(ix + 2)
+			ret	z	; if you can find a free memory location (ix - addres of free memory)
+			inc	ix
+			jr	LL_Malloc1
 Text 			defb	"Hello World", 0
 AdrOfNode		defw	0	
 LinkedList		defw	NULL  ; address of the initial element of the linked list in memory
